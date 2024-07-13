9642645407c3aad5fd3d7b8e0d40566c898e61be
Philip osir Otieno <philiposir2@gmail.com>
2024-07-13 17:35:36 +0300

    fetch labs

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  let formData = {
+    name: userName,
+    email: userEmail
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch('http://localhost:3000/users', configObj)
+    .then(response => response.json())
+    .then(data => {
+      appendIdToDOM(data.id);
+    })
+    .catch(error => {
+      appendErrorToDOM(error.message);
+    });
+}
+
+function appendIdToDOM(id) {
+  document.body.innerHTML += `<p>New user ID: ${id}</p>`;
+}
+
+function appendErrorToDOM(message) {
+  document.body.innerHTML += `<p>Error: ${message}</p>`;
+}
